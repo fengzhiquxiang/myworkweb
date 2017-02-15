cf7cea2a6a860b5a472d845e2500676c50a8c4df
zgl <fengzhiquxiang@gmail.com>
2017-02-15 12:17:27 +0800

    add custom

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	// "fmt"
-	// "html"
 	"html/template"
 	"log"
 	"net/http"
 )
+
+type custom struct {
+	id          int
+	fullname    string
+	nickname    string
+	address     string
+	tradetime   int
+}
+
+type product struct {
+	name    string
+	model    string
+}
 
 var tpl *template.Template
 
@@ -15,12 +26,15 @@ func init() {
 }
 
 func main() {
-	// http.Handle("/foo", fooHandler)
+	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
+	http.Handle("/js/", http.StripPrefix("/js", http.FileServer(http.Dir("./js"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-
 		tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	})
+
+	http.HandleFunc("/submit_input_data", func(w http.ResponseWriter, r *http.Request) {
+		tpl.ExecuteTemplate(w, "index.gohtml", "jquery ajax data back!!")
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
