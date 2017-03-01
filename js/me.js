52c6350b7ffbef37b03611309b63ba3dbc9e98d7
zgl <fengzhiquxiang@gmail.com>
2017-03-01 16:30:01 +0800

    add update custom and modify bson to json in struct which cause mistake key capture

--- js/me.js
+++ js/me.js
@@ -1,7 +1,27 @@
-$("#ajax").click(fuction(){
-	alert("ajax start");
-    $.post("/ajax", function(data){
-	  // $( "#ajax" ).html( data );
-	  alert("ajax success");
+function ajaxtest(){
+  alert("ajax start");
+    // $.post("/get_customs",
+    // {
+    //   name: "Donald Duck",
+    //   city: "Duckburg"
+    // },
+    // function(data,status){
+    //     alert("Data: " + data.address + "\nStatus: " + status);
+    // });
+
+    $.getJSON( "/get_customs", function( data ) {
+	  var items = [];
+	  $.each( data, function( key0, val0 ) {
+	    // items.push( "<li id='" + key0 + "'>" + key0 + ":" + val0 + "</li>" );
+	    $.each( val0, function( key1, val1 ) {
+		  items.push( "<li id='" + key1 + "'>" + key1 + ":" + val1 + "</li>" );
+		});
+		items.push("<br/>")
+	  });
+	 
+	  $( "<ul/>", {
+	    "class": "my-new-list",
+	    html: items.join( "" )
+	  }).appendTo( "body" );
 	});
-});
+}
--- main.go
+++ main.go
@@ -16,10 +16,10 @@ import (
 type Custom struct {
 	Cid         string        `bson:"cid" json:"cid"`
 	Fullname    string        `bson:"fullname" json:"fullname"`
-	Nickname    string        `bson:"nickname,omitempty" json:"nickname,omitempty"`
-	Address     string        `bson:"address,omitempty" bson:"address,omitempty"`
-	Phone       string        `bson:"phone,omitempty" bson:"phone,omitempty"`
-	Email       string        `bson:"email,omitempty" bson:"email,omitempty"`
+	Nickname    string        `bson:"nickname" json:"nickname"`
+	Address     string        `bson:"address" json:"address"`
+	Phone       string        `bson:"phone" json:"phone"`
+	Email       string        `bson:"email" json:"email"`
 }
 
 type product struct {
@@ -36,6 +36,7 @@ func init() {
 func main() {
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
 	http.Handle("/js/", http.StripPrefix("/js", http.FileServer(http.Dir("./js"))))
+	http.Handle("/jquery-easyui-1.5.1/", http.StripPrefix("/jquery-easyui-1.5.1", http.FileServer(http.Dir("./jquery-easyui-1.5.1"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tpl.ExecuteTemplate(w, "index.gohtml", nil)
@@ -52,15 +53,23 @@ func main() {
 	    defer session.Close()
 	    c := session.DB("mywebdb").C("customs")
     	var results []Custom
-        err = c.Find(bson.M{}).All(&results)
+        // err = c.Find(bson.M{}).All(&results)
+        err = c.Find(nil).All(&results)
         if err != nil {
                 log.Fatal(err)
         }
         fmt.Println(results)
-		if err = json.NewEncoder(w).Encode(results); err != nil {
-			log.Fatal(err)
+		// if err = json.NewEncoder(w).Encode(results); err != nil {
+		// 	log.Fatal(err)
+		// }
+		js, err := json.Marshal(results)
+		if err != nil {
+		  http.Error(w, err.Error(), http.StatusInternalServerError)
+		  return
 		}
-		// w.WriteHeader(http.StatusOK) // 200
+        // fmt.Println(js)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
 	})
 
 	http.HandleFunc("/add_custom", func(w http.ResponseWriter, r *http.Request) {
@@ -87,6 +96,53 @@ func main() {
 	    defer session.Close()
 	    c := session.DB("mywebdb").C("customs")
         err = c.Insert(&Custom{cid, fullname,nickname,address,phone,email})
+        // err = c.Insert(bson.M{"cid":cid, "fullname":fullname,"nickname":nickname,"address":address,"phone":phone,"email":email})
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        //give result to ajax success function receive then it refresh block
+        results := make(map[string]int)
+    	results["errorMsg"] = 0
+
+		js, err := json.Marshal(results)
+		if err != nil {
+		  http.Error(w, err.Error(), http.StatusInternalServerError)
+		  return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
+
+	http.HandleFunc("/update_custom", func(w http.ResponseWriter, r *http.Request) {
+		// tpl.ExecuteTemplate(w, "index.gohtml", "jquery ajax data back!!")
+		ocid := r.FormValue("ocid")
+
+		fmt.Println(ocid)
+
+		cid := r.FormValue("cid")
+		fullname := r.FormValue("fullname")
+		nickname := r.FormValue("nickname")
+		address := r.FormValue("address")
+		phone := r.FormValue("phone")
+		email := r.FormValue("email")
+
+		fmt.Println(cid)
+		fmt.Println(fullname)
+		fmt.Println(nickname)
+		fmt.Println(address)
+		fmt.Println(phone)
+		fmt.Println(email)
+
+		// Connect to our local mongo
+		session, err := mgo.Dial("mongodb://localhost")
+		if err != nil {
+	            panic(err)
+	    }
+	    defer session.Close()
+	    c := session.DB("mywebdb").C("customs")
+        err = c.Update(bson.M{"cid":ocid},bson.M{"cid":cid, "fullname":fullname,"nickname":nickname,"address":address,"phone":phone,"email":email})
         if err != nil {
                 log.Fatal(err)
         }
@@ -137,19 +193,22 @@ func main() {
 
 	http.HandleFunc("/ajax", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.RequestURI)
-		
-		// Connect to our local mongo
-		session, err := mgo.Dial("mongodb://localhost")
+		p1 := r.FormValue("name")
+		p2 := r.FormValue("city")
+		fmt.Println(p1) 
+		fmt.Println(p2) 
+
+		results := make(map[string]int)
+    	results["success"] = 200
+
+		js, err := json.Marshal(results)
 		if err != nil {
-	            panic(err)
-	    }
-	    defer session.Close()
-	    c := session.DB("mywebdb").C("customs")
-		fmt.Println(c)
-        err = c.Insert(&Custom{r.RequestURI, r.RequestURI,"","","",""})
-        if err != nil {
-                log.Fatal(err)
-        }
+		  http.Error(w, err.Error(), http.StatusInternalServerError)
+		  return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
